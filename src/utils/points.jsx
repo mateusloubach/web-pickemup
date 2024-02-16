f867ce003853b2dbc86cf184cb4c99b1f8fe690e
mateusloubach <mcesarloubach@gmail.com>
2024-02-16 17:44:25 -0300

    Update - coords

--- src/utils/points.jsx
+++ src/utils/points.jsx
@@ -8,14 +8,14 @@ function randomPoint(centerLong, centerLat) {
     return [long, lat]
 }
 
-let userPoint = randomPoint(-48.2622, -18.9113)
+let userPoint = randomPoint(-48.326792, -18.964537)
 let user = { point: userPoint, contact: "+55 (31) 99432-9234", time: "Sexta-Feira, 16 de Fev. 2024", token: "12345" }
 
 
 let points = [user]
 
 for (let i = 0; i < 10; i++) {
-    let point = randomPoint(-48.2622, -18.9113)
+    let point = randomPoint(-48.326792, -18.909710)
 
     let details = { point, contact: "+55 (31) 99432-9234", time: "Sexta-Feira, 16 de Fev. 2024"
     }
